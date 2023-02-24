43a51084b468ccd580524f0005404a65c55d9a88
Brian Hopkins <brianhh1230@gmail.com>
2023-02-23 19:03:33 -0500

    feat(initial-files): adding in the initial files

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.11.1-alpine
+
+WORKDIR /app
+
+ENV DISCORD_TOKEN=""
+ENV RASA_URL=""
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+COPY bot.py .
+
+CMD ["python3", "-m", "bot.py"]
---
+++ bot.py
@@ -0,0 +1,45 @@
+import discord
+import os
+import requests
+import json
+
+discord_token = os.getenv("DISCORD_TOKEN")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('<@1078452054084825220>'):
+            jasper_url = os.getenv('RASA_URL')
+            discord_message = message.content.split(" ", 1)[1]
+            headers = {
+                'Content-Type': 'application/json',
+            }
+
+            json_data = {
+                'sender': message.author.name,
+                'message': discord_message,
+            }
+
+            print(json_data)
+
+            response = requests.post(jasper_url, headers=headers, json=json_data)
+            parsed = json.loads(response.content)
+            print(parsed)
+            jasper_response = parsed[0]['text']
+            print(jasper_response)
+            # Send response from Jasper
+            await message.reply(jasper_response, mention_author=True)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(discord_token)
