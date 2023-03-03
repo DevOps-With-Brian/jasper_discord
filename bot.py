ce767e2e3c8add079e8787fa2380ce2d1b5f7a51
Brian Hopkins <brianhh1230@gmail.com>
2023-03-02 20:53:25 -0500

    feat(jasper): updating the jasper connector to be able to respond more to bot_testing channel

--- bot.py
+++ bot.py
@@ -8,6 +8,51 @@ discord_token = os.getenv("DISCORD_TOKEN")
 REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
 
 class MyClient(discord.Client):
+    def call_rasa(self, message):
+        if message.content.startswith('<@1078452054084825220>'):
+            if message.author.id == self.user.id:
+                return
+            jasper_url = os.getenv('RASA_URL_LOCAL')
+
+            discord_message = message.content.split(" ", 1)[1]
+            headers = {
+                'Content-Type': 'application/json',
+            }
+
+            json_data = {
+                'sender': message.author.name,
+                'message': discord_message,
+            }
+
+            response = requests.post(jasper_url, headers=headers, json=json_data)
+            parsed = json.loads(response.content)
+
+            if parsed == []:
+                return
+            jasper_response = parsed[0]['text']
+            return jasper_response
+        else:
+            if message.author.id == self.user.id:
+                return
+            jasper_url = os.getenv('RASA_URL_LOCAL')
+            discord_message = message.content
+            headers = {
+                'Content-Type': 'application/json',
+            }
+
+            json_data = {
+                'sender': message.author.name,
+                'message': discord_message,
+            }
+
+            response = requests.post(jasper_url, headers=headers, json=json_data)
+            parsed = json.loads(response.content)
+
+            if parsed == []:
+                return
+            jasper_response = parsed[0]['text']
+            return jasper_response
+
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
@@ -16,6 +61,25 @@ class MyClient(discord.Client):
         # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
+        
+        if (str(message.channel.id) == "1078462751309049896" and message.content.startswith('<@1078452054084825220>') == False):
+            jasper_response = self.call_rasa(message)
+            oos = False
+            if jasper_response == "oos":
+                oos = True
+                
+            if oos == False:
+                await message.reply(jasper_response, mention_author=True)
+        
+        if message.content.startswith('<@1078452054084825220>'):
+            jasper_response = self.call_rasa(message)
+            oos = False
+            if jasper_response == "oos":
+                oos = True
+
+            if oos == False:
+                await message.reply(jasper_response, mention_author=True)
+
 
         if (message.channel.name != 'diffusion_generation'):
              if message.content.startswith('!diffusion'):
@@ -67,29 +131,6 @@ class MyClient(discord.Client):
                 output = version.predict(**inputs)
                 print(output)
                 await message.channel.send(output[0], mention_author=True)
-            
-
-        if message.content.startswith('<@1078452054084825220>'):
-            jasper_url = os.getenv('RASA_URL')
-            discord_message = message.content.split(" ", 1)[1]
-            headers = {
-                'Content-Type': 'application/json',
-            }
-
-            json_data = {
-                'sender': message.author.name,
-                'message': discord_message,
-            }
-
-            print(json_data)
-
-            response = requests.post(jasper_url, headers=headers, json=json_data)
-            parsed = json.loads(response.content)
-            print(parsed)
-            jasper_response = parsed[0]['text']
-            print(jasper_response)
-            # Send response from Jasper
-            await message.reply(jasper_response, mention_author=True)
 
 
 intents = discord.Intents.default()
